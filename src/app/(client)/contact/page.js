029bc1ba442381499b79928f72f3bd0ebe006d98
omkar <omkar13021@gmail.com>
2024-08-14 01:57:15 +0530

    contact page

--- src/app/(client)/contact/page.js
+++ src/app/(client)/contact/page.js
@@ -1,8 +1,88 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+import styles from '../_lib/styles/contace.module.css';
+
 function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const add = (e) => {
+        e.preventDefault();
+        const newMessage = { name, email, message };
+        postMessage(newMessage);
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
+    const postMessage = async (newMessage) => {
+        try {
+            const response = await axios.post('/api/contact', newMessage);
+            console.log(response);
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    }
+
     return (
-        <div>
-            Contact
-        </div>
+        <section className={styles.contactSection}>
+            <div className={styles.contactContainer}>
+                <div className={styles.socialMediaLinks}>
+                    <a href="https://x.com/omkar15183788" target="_blank" rel="noopener noreferrer">
+                        <img src='/assets/icons/twitter.png' alt="Twitter" />
+                    </a>
+                    <a href="https://www.instagram.com/onkaar._" target="_blank" rel="noopener noreferrer">
+                        <img src='/assets/icons/instagram.png' alt="Instagram" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/omkarpanchalcse" target="_blank" rel="noopener noreferrer">
+                        <img src='/assets/icons/linkedin.png' alt="Linkedin" />
+                    </a>
+                    <a href="https://github.com/omkar755292" target="_blank" rel="noopener noreferrer">
+                        <img src='/assets/icons/github.png' alt="GitHub" />
+                    </a>
+                    <a href="mailto:omkarpanchal.cse@gmail.com" target="_blank" rel="noopener noreferrer">
+                    <img src='/assets/icons/email.png' alt="Email" />
+                    </a>
+                </div>
+                {/* 780FFF */}
+                <div>
+                    <h2>Contact Me</h2>
+                    <img src='/assets/images/avatar.svg' alt="Avatar" />
+                </div>
+                <form className={styles.contactForm} onSubmit={add}>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder='Name'
+                        required />
+
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Email'
+                        required />
+
+                    <textarea
+                        id="message"
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        rows="4"
+                        placeholder='Message'
+                        required></textarea>
+
+                    <button type="submit">Send Message</button>
+                </form>
+            </div>
+        </section>
     );
 }
 
