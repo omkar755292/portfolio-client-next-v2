9947da9664af4bf165cd5082df44fce6cac2241d
omkar <omkar13021@gmail.com>
2024-08-14 12:42:28 +0530

    added responsive design

--- src/app/(client)/_lib/components/Header.js
+++ src/app/(client)/_lib/components/Header.js
@@ -1,7 +1,16 @@
+"use client"
 import Link from "next/link";
 import styles from '../styles/header.module.css';
+import { useState } from "react";
 
 const Header = () => {
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
     return (
         <div className={styles.header}>
             <div className={styles.navbar}>
@@ -10,14 +19,16 @@ const Header = () => {
                         <img src="/assets/images/logo.svg" alt="Logo" />
                     </Link>
                 </div>
-                <div className={styles.navlink}>
+                <div className={`${styles.navlink} ${isNavOpen ? styles.displayNone : ''}`}>
                     <Link href="/" aria-label="Home">Home</Link>
                     <Link href="/about" aria-label="About Me">About Me</Link>
                     <Link href="/blogs" aria-label="Blogs">Blogs</Link>
                     <Link href="/projects" aria-label="Projects">Projects</Link>
                     <Link href="/contact" aria-label="Contact Me">Contact Me</Link>
+                    <button className={styles.burger} onClick={toggleNav}>
+                        <img src="/assets/icons/arrowdown.png" alt="downarrow" />
+                    </button>
                 </div>
-
                 <Link href="/businessInquiry" aria-label="Business Inquiry">
                     <button className={styles.businessInquiryButton}>Business Inquiry</button>
                 </Link>
--- src/app/(client)/page.js
+++ src/app/(client)/page.js
@@ -12,15 +12,16 @@ const Home = () => {
             modules that lead to the success of the overall product. <br /> I design and code beautifully simple things, and I
             love what I do.
           </p>
+
+          <div className={styles.downloadResume}>
+            <a href='/assets/omkarpanchal_resume.pdf' target="_blank" rel="noopener noreferrer">
+              <img className={styles.downloadIcon} src='/assets/icons/file.png' alt='Download icon' /> Resume
+            </a>
+          </div>
+          
           <img src='/assets/images/laptop.svg' alt="laptop image" />
         </div>
 
-
-        <div className={styles.downloadResume}>
-          <a href='/assets/omkarpanchal_resume.pdf' target="_blank" rel="noopener noreferrer">
-            <img className={styles.downloadIcon} src='/assets/icons/file.png' alt='Download icon' /> Resume
-          </a>
-        </div>
       </section>
       <hr />
     </main>
